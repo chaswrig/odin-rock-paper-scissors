d80a29b4791985d711e98e2d531b5fd9dd85958f
chaswrig <42904530+chaswrig@users.noreply.github.com>
2025-01-24 15:19:13 -0600

    Minimum game logic. Deliberately bad for future improvement.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,66 @@
-console.log(getHumanChoice())
+//Scripts for playing the game.
+
+let humanScore = 0;
+let computerScore = 0;
+
+playRound();
+
+function playRound(){
+    let computerChoice = getComputerChoice();
+    let humanChoice = getHumanChoice();
+
+    determineWinner(computerChoice, humanChoice);
+}
+
+function determineWinner(computerChoice, humanChoice){
+    if(computerChoice === humanChoice){
+        console.log("Tie!");
+    } else if (computerChoice === "rock") {
+        let outcome = "";
+        switch(humanChoice){
+            case "paper":
+                outcome = "Humans win!";
+                break;
+            
+            case "scissors":
+                outcome = "Computers win!";
+                break;
+            default: outcome = "Tie after all?"
+        }
+            
+        console.log(outcome);
+    } else if (computerChoice === "paper") {
+        let outcome = "";
+        switch(humanChoice){
+            case "scissors":
+                outcome = "Humans win!";
+                break;
+            
+            case "rock":
+                outcome = "Computers win!";
+                break;
+            default: outcome = "Tie after all?"
+        }
+            
+        console.log(outcome);
+    } else {
+        let outcome = "";
+        switch(humanChoice){
+            case "rock":
+                outcome = "Humans win!";
+                break;
+            
+            case "paper":
+                outcome = "Computers win!";
+                break;
+            default: outcome = "Tie after all?"
+
+        console.log(outcome);
+        }
+    }
+    
+}
+
 
 function getComputerChoice(){
 
